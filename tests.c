71024906855a310117c865d73fa86fd5e7308a71
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-01-31 15:11:17 +0700

    Added matricies 2x2

--- tests.c
+++ tests.c
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include <stdio.h>
 #define _VMATH_IMPLEMENTATION_
 #include "vmath.h"
 
-int main() {
+void test_vectors() {
+    printf("Testing Vectors.. ");
     float first[] =  { 1.0f, 6.0f  };
     float second[] = { 2.0f, 4.0f  };
     float expected[] = { 3.0f, 10.0f };
@@ -13,5 +15,29 @@ int main() {
     expected[0] = 5.0f; expected[1] = 30.0f;
     VM2_ScaleO(first, 5.0, result);
     assert(VM2_Eq(result, expected));
+
+    printf("OK\n");
+}
+
+void test_matricies() {
+    printf("Testing Matricies.. ");
+    float first[4] = VM22_IDENTITY;
+    float second[4] = { 1.0, 2.0, 3.0, 4.0 };
+    float expected[4] = {2.0, 2.0, 3.0, 5.0};
+    float result[4];
+
+    VM22_AddO(first, second, result);
+    assert(VM22_Eq(result, expected));
+
+    VM22_ScaleO(first, 2, result);
+    VM22_Set(expected, 2., 0., 0., 2.);
+    assert(VM22_Eq(result, expected));
     
+    printf("OK\n");
+}
+
+
+int main() {
+    test_vectors();
+    test_matricies();
 }
--- vmath.h
+++ vmath.h
@@ -9,19 +9,55 @@
 // other functions either return value or
 // output it into the first argument
 
+// Vector2
 void  VM2_AddO(float*, float*, float* out);
-void  VM2_Add(float*, float*);
+void  VM2_Add(float* to, float*);
+
 void  VM2_SubO(float*, float*, float* out);
-void  VM2_Sub(float*, float*);
+void  VM2_Sub(float* to, float*);
+
 void  VM2_ScaleO(float*, float, float* out);
-void  VM2_Scale(float*, float);
+void  VM2_Scale(float* to, float);
+
 void  VM2_DotO(float*, float*, float* out);
 float VM2_Dot(float*, float*);
+
 void  VM2_AreaO(float*, float*, float* out);
 float VM2_Area(float*, float*);
-void  VM2_Copy(float*, float* dest);
-_Bool VM2_Eq(float*, float*);
 
+void  VM2_LengthO(float*, float*);
+float VM2_Length(float*);
+
+void  VM2_Copy(float* to, float* from);
+_Bool VM2_Eq(float*, float*);
+void VM2_Set(float*, float, float);
+
+
+// Matrix 2x2
+void  VM22_AddO(float*, float*, float* out);
+void  VM22_Add(float* to, float*);
+
+void  VM22_SubO(float* from, float*, float* out);
+void  VM22_Sub(float* to, float*);
+
+void  VM22_ScaleO(float*, float, float* out);
+void  VM22_Scale(float*, float);
+
+void  VM22_Copy(float* to, float* from);
+_Bool VM22_Eq(float*, float*);
+
+void  VM22_Transform(float* vec, float* mat);
+void  VM22_TransformO(float* vec, float* mat, float* out);
+void  VM22_Identity(float*);
+void  VM22_InverseO(float*, float* out);
+void  VM22_Inverse(float*);
+void  VM22_DetO(float*, float* out);
+float VM22_Det(float*);
+void  VM22_Set(float*, float, float, float, float);
+
+
+#define VM22_IDENTITY {1.0, 0.0, \
+                        0.0, 1.0}
 
 //#endif
 #ifdef _VMATH_IMPLEMENTATION_
@@ -34,8 +70,7 @@ void VM2_AddO(float* first, float* second, float* out) {
 void VM2_Add(float* first, float* second) {
     float temp[2];
     VM2_AddO(first, second, temp);
-    VM2_Copy(temp, first);
-
+    VM2_Copy(first, temp);
 }
 
 void VM2_SubO(float* first, float* second, float* out) {
@@ -46,7 +81,7 @@ void VM2_SubO(float* first, float* second, float* out) {
 void VM2_Sub(float* first, float* second) {
     float temp[2];
     VM2_SubO(first, second, temp);
-    VM2_Copy(temp, first);
+    VM2_Copy(first, temp);
 }
 
 void VM2_ScaleO(float* first, float scale, float* out) {
@@ -57,7 +92,7 @@ void VM2_ScaleO(float* first, float scale, float* out) {
 void VM2_Scale(float* first, float scale) {
     float temp[2];
     VM2_ScaleO(first, scale, temp);
-    VM2_Copy(temp, first);
+    VM2_Copy(first, temp);
 }
 
 float VM2_Dot(float* first, float* second) {
@@ -77,12 +112,98 @@ void VM2_AreaO(float* first, float* second, float* out) {
 }
 
 void VM2_Copy(float* first, float* second) {
-    second[0] = first[0];
-    second[1] = first[1];
+    first[0] = second[0];
+    first[1] = second[1];
 }
 
 _Bool VM2_Eq(float* first, float* second) {
     return first[0] == second[0] && first[1] == second[1];
 }
+
+void VM2_Set(float* to, float x, float y) {
+    to[0] = x;
+    to[1] = y;
+}
+
+// Matrix 2x2
+
+void VM22_AddO(float* first, float* second, float* out) {
+    for (int i = 0; i < 4; i++) {
+	out[i] = first[i] + second[i];
+    }
+}
+
+void  VM22_Add(float* to, float* from) {
+    float temp[4] = {0, 0, 0, 0};
+    VM22_AddO(to, from, temp);
+    VM22_Copy(to, temp);
+}
+
+void VM22_SubO(float* first, float* second, float* out) {
+    for (int i = 0; i < 4; i++) {
+	out[i] = first[i] - second[i];
+    }
+}
+
+void  VM22_Sub(float* to, float* a) {
+    float temp[4] = {0, 0, 0, 0};
+    VM22_AddO(to, a, temp);
+    VM22_Copy(to, temp);
+}
+
+void VM22_ScaleO(float* first, float second, float* out) {
+    for (int i = 0; i < 4; i++) {
+	out[i] = first[i] * second;
+    }
+}
+
+void  VM22_Scale(float* to, float a) {
+    float temp[4] = {0, 0, 0, 0};
+    VM22_ScaleO(to, a, temp);
+    VM22_Copy(to, temp);
+}
+
+void  VM22_Copy(float* to, float* from) {
+    for (int i = 0; i < 4; i++) {
+	to[i] = from[i];
+    }
+}
+
+_Bool VM22_Eq(float* first, float* second) {
+    for (int i = 0; i < 4; i++) {
+	if (first[i] != second[i]) return 0;
+    }
+    return 1;
+}
+
+void VM22_Set(float* to, float x, float y, float z, float w) {
+    to[0] = x;
+    to[1] = y;
+    to[2] = z;
+    to[3] = w;
+}
+
+void VM22_Transform(float* vec, float* mat);
+void VM22_TransformO(float* vec, float* mat, float* out);
+
+void VM22_Identity(float* to) {
+    float temp[4] = VM22_IDENTITY;
+    VM22_Copy(to, temp);
+}
+
+void VM22_InverseO(float*, float* out);
+void VM22_Inverse(float*);
+
+void VM22_DetO(float* of, float* out) {
+    VM2_AreaO(of, of+2, out);
+}
+
+float VM22_Det(float* of) {
+    float temp[4];
+    VM22_DetO(of, temp);
+    VM22_Copy(of, temp);
+}
+
+
 
 #endif
