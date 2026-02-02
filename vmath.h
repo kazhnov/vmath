484c6e60a38b35b196d8032b282870dd1a1b38c5
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-02-02 16:28:21 +0700

    Added VM3_Copy

--- vmath.h
+++ vmath.h
@@ -11,6 +11,15 @@
 // other functions either return value or
 // output it into the first argument
 
+// Scalar
+float VM_InverseSqrt(float);
+
+// Variable Vectors
+_Bool VMV_Eq(float* first, float* second, uint32_t dims);
+void VMV_Copy(float* first, float* second, uint32_t dims);
+
+
+
 // Vector2
 void  VM2_AddO(float*, float*, float* out);
 void  VM2_Add(float* to, float*);
@@ -30,22 +39,31 @@ float VM2_Area(float*, float*);
 void  VM2_LengthO(float*, float*);
 float VM2_Length(float*);
 
+float VM2_LengthSquared(float*);
+float VM2_InverseLength(float*);
+
 void VM2_RotateO(float*, float, float* out);
 void VM2_Rotate(float*, float);
 
-void  VM2_Copy(float* to, float* from);
+//void  VM2_Copy(float* to, float* from);
+#define VM2_Copy(to, from) (VMV_Copy(to, from, 2))
 _Bool VM2_Eq(float*, float*);
+
 void VM2_Set(float*, float, float);
 
 void VM2_Pack(float* values, uint32_t amount, float (*out)[2]);
 void VM2_Unpack(float (*values)[2], uint32_t amount, float* out);
 _Bool VM2P_Eq(float (*first)[2], float (*second)[2], uint32_t amount);
 
-_Bool VMV_Eq(float* first, float* second, uint32_t amount);
-
+#define VM3_Eq(first, second) (VMV_Eq(first, second, 3))
+#define VM3_Copy(to, from)    (VMV_Copy(to, from, 3))
 
 #define VM2_ZERO {0.0f, 0.0f}
 #define VM2_ONE  {1.0f, 1.0f}
+
+// Vector3
+
+
 
 // Matrix 2x2
 void  VM22_AddO(float*, float*, float* out);
@@ -75,6 +93,23 @@ void  VM22_Set(float*, float, float, float, float);
 
 //#endif
 #ifdef _VMATH_IMPLEMENTATION_
+
+// the infamous quake quick inverse square root function (Comments preserved)
+float VM_InverseSqrt(float number) {
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;                       // evil floating point bit level hacking
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+    return y;
+}
 
 void VM2_AddO(float* first, float* second, float* out) {
     out[0] = first[0] + second[0];
@@ -125,16 +160,24 @@ void VM2_AreaO(float* first, float* second, float* out) {
     out[0] = VM2_Area(first, second);
 }
 
-void VM2_Copy(float* first, float* second) {
-    first[0] = second[0];
-    first[1] = second[1];
+/* void VM2_Copy(float* first, float* second) { */
+/*     first[0] = second[0]; */
+/*     first[1] = second[1]; */
+/* } */
+
+float VM2_LengthSquared(float* v) {
+    return v[0]*v[0] + v[1]*v[1];
 }
 
+float VM2_InverseLength(float* v) {
+    return VM_InverseSqrt(VM2_LengthSquared(v));
+}
 
 void VM2_RotateO(float* in, float angle, float* out) {
     out[0] = in[0] * cosf(angle) - in[1] * sinf(angle);
     out[1] = in[0] * sinf(angle) + in[1] * cosf(angle);
 }
+
 void VM2_Rotate(float* to, float angle) {
     float temp[2];
     VM2_RotateO(to, angle, temp);
@@ -169,13 +212,19 @@ _Bool VM2P_Eq(float (*first)[2], float (*second)[2], uint32_t amount) {
     return 1;
 }
 
-_Bool VMV_Eq(float* first, float* second, uint32_t amount) {
-    for (int i = 0; i < amount; i++) {
+_Bool VMV_Eq(float* first, float* second, uint32_t dims) {
+    for (int i = 0; i < dims; i++) {
 	if (first[i] != second[i]) return 0;
     }
     return 1;
 }
 
+
+void VMV_Copy(float* to, float* from, uint32_t dims) {
+    for (int i = 0; i < dims; i++) {
+	to[i] = from[i];
+    }
+}
 
 // Matrix 2x2
 
