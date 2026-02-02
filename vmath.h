d972b0fe1dae7893d94ee8e556bb980f77ead0a9
Vanya Kazhnoff <iwan.kasinowe@gmail.com>
2026-02-02 17:30:59 +0700

    some debug stuff

--- vmath.h
+++ vmath.h
@@ -346,7 +346,8 @@ void VM44_V3A3(float* pos, float* a, float* out) {
 void VM44_V2A1(float* pos, float angle, float* out) {
     float s = sinf(angle);
     float c = cosf(angle);
-
+    printf("s: %f, c: %f\n");
+    
     VM4_Set(out+0,  c, -s, 0, pos[0]);
     VM4_Set(out+4,  s,  c, 0, pos[1]);
     VM4_Set(out+8,  0,  0, 1, 0     );
